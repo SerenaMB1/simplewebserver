a5b92495a642017e893067686eb2c59c1d1ac32b
Serena Badesha <sbadesha@gmail.com>
2021-08-27 12:40:39 +0100

    simple webserver

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/hello", func(writter http.ResponseWriter, request *http.Request) {
-
+	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("hello world!"))
 	})
 
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
